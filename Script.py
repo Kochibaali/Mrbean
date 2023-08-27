27adf120f47717ecada0be6c29095924f173430b
Heyboy2004 <113125972+Heyboy2004@users.noreply.github.com>
2023-08-28 01:06:44 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -5,6 +5,9 @@ class script(object):
     HELP_TXT = """<b>𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂.</b>"""
 
+    PEPE_TXT = """<b>𝙷𝙴𝚈 {}
+ninakk oru kunnayum illa malar.</b>"""
+ 
 
 # ⚠️ Please don't change our credits 𝚃𝙷𝙰𝙽𝙺𝚂 𝚃𝙾 & 𝙳𝙴𝚅 👇🏻
 
