1609cbc74cfa6213c207641128186bc2e3de7bba
Heyboy2004 <113125972+Heyboy2004@users.noreply.github.com>
2023-08-28 10:58:16 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1350,16 +1350,19 @@ async def advantage_spell_chok(client, msg):
     movielist = []
     if not movies:
         reqst_gle = mv_rqst.replace(" ", "+")
-        buttons = [[
-            InlineKeyboardButton('NOTHING', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-        ], [
-            InlineKeyboardButton('TAM', url='https://t.me/sk_movies12'),
-            InlineKeyboardButton('TEL', url='https://t.me/sk_movies12'),
-            InlineKeyboardButton('HIN', url='https://t.me/sk_movies12'),
-            InlineKeyboardButton('ENG', url='https://t.me/sk_movies12'),
-            InlineKeyboardButton('MAL', url='https://t.me/sk_movies12')
-        ]]        
-        k=await msg.reply("<b>𝖲ᴏʀʀʏ 𝖭ᴏ 𝖥ɪʟᴇ𝗌 𝖶ᴇʀᴇ 𝖥ᴏᴜɴᴅ.\n\n𝖢ʜᴇᴄᴋ 𝖸ᴏᴜʀ 𝖲ᴘᴇʟʟɪɴɢ ɪɴ 𝖦ᴏᴏɢʟᴇ ᴀɴᴅ 𝖳ʀʏ 𝖠ɢᴀɪɴ. ♻️\n\n𝖱ᴇᴀᴅ 𝖨ɴ𝗌ᴛʀᴜᴄᴛɪᴏɴ𝗌 ғᴏʀ ʙᴇᴛᴛᴇʀ 𝖱ᴇ𝗌ᴜʟᴛ𝗌 👇🏻</b>", reply_markup=InlineKeyboardMarkup(buttons))    
+        btn_eng = InlineKeyboardButton("ENG", callback_data="eng")
+btn_mal = InlineKeyboardButton("MAL", callback_data="mal")
+btn_hin = InlineKeyboardButton("HIN", callback_data="hin")
+btn_tam = InlineKeyboardButton("TAM", callback_data="tam")
+
+language_row = [btn_eng, btn_mal, btn_hin, btn_tam]
+btn_google = InlineKeyboardButton("Search on Google", url="https://www.google.com/")
+
+google_row = [btn_google]
+
+keyboard = InlineKeyboardMarkup(inline_keyboard=[language_row, google_row])
+
+    k = await msg.reply_text(text=script.PEPE_TXT, reply_markup=keyboard)
         await asyncio.sleep(20)
         await k.delete()
         await msg.delete()
